5a9283ea2714f5849ca9a6e6ea37fa804f36f499
jdblair78 <jdblair78@gmail.com>
2026-02-26 13:11:45 -0500

    api int

--- src/pages/Genres.jsx
+++ src/pages/Genres.jsx
@@ -1,21 +1,33 @@
-import React from "react";
 import "./genres.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Genre = () => {
+const Genre = (searchTerm) => {
+  const [movies, setMovies] = useState([])
+  async function fetchMovies() {
+    const apiKey = "ab612e22"
+  const { data } = await axios.get(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`)
+  setMovies(data)
+  console.log(data)
+}
+  useEffect(() => {
+fetchMovies();
+  }, [])
+
   return (
     <>
     <div className="genres">
       <div className="container">
         <div className="row">
-          <h3 className="genre__title">Today's Top Picks for You</h3>
+          <h4 className="genre__title">Today's Top Picks for You</h4>
+          <div className="top__picks">
+            <img src={searchTerm.Poster} className="genre__img" alt=""></img>
+          </div>
+          <h4 className="genre__title">Makes You Laugh</h4>
           <div className="top__picks">
             <img src="#" className="genre__img" alt=""></img>
           </div>
-          <h3 className="genre__title">Makes You Laugh</h3>
-          <div className="top__picks">
-            <img src="#" className="genre__img" alt=""></img>
-          </div>
-          <h3 className="genre__title">Love Suspense</h3>
+          <h4 className="genre__title">Love Suspense</h4>
           <div className="top__picks">
             <img src="#" className="genre__img" alt=""></img>
           </div>
